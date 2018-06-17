4094604189325e85e39c438b24dcc425b2c2432d
mdaniloraulino <38267066+mdaniloraulino@users.noreply.github.com>
2018-06-17 19:13:35 -0300

    piton

--- Python/MaxShowTentativa3.py
+++ Python/MaxShowTentativa3.py
@@ -13,21 +13,22 @@ import numpy as np
 
 
 class Vertice(object):
-    palco = 0
-    
-    def __init__(self,id,estilo):
+        
+    def __init__(self,id,estilo,horario):
         self.id = id
         self.estilo = estilo
+        self.horario = horario
         self.adj = list()
+        self.dia = int()
         
     def __repr__(self):
-        return f'{self.id}  {len(self.adj)}'
+        return f'{self.id}|dia {self.dia}'
     
     def __eq__(self,v):
        if isinstance(v,self.__class__):
             return self.id == v.id and self.estilo == v.estilo
-#    def __cmp__(self, other):
-#        return self.adj.count(1).__cmp__(other.adj.count(1))
+    def __cmp__(self, other):
+        return len(self.adj).__cmp__(len(other.adj))
     def __lt__(self,v):
         return len(self.adj) < len(v.adj)
     
@@ -35,7 +36,6 @@ class Vertice(object):
        if isinstance(v,self.__class__):
             return self.id != v.id and self.estilo != v.estilo
         
-    
 
 class Aresta():
     def __init__(self,id):
@@ -49,25 +49,20 @@ class Aresta():
     
 vertices = []
 arestas = []
+cores = tuple([1,2,3,4,5,6,7,8,9])
 
 file = open('bandas.txt','r') 
 
 for line in file:
     banda = line.split(',')
-    banda[1] = banda[1][:-1]
-    vertices.append(Vertice(banda[0],banda[1]))
-    
-for i in vertices:
-    for j in vertices:
-        if i.adj == j.adj:
-            print('toma no cu')
-
+    banda[2] = banda[2][:-1]
+    vertices.append(Vertice(banda[0],banda[1],banda[2]))    
 
 for i in vertices:
     for j in vertices:
         if i == j:
             continue
-        elif i.estilo == j.estilo:
+        elif i.estilo == j.estilo or i.horario == j.horario:
             aresta = Aresta([i,j])
             if aresta in arestas:
                 continue
@@ -77,11 +72,37 @@ for i in vertices:
                 i.adj.append(j)
             arestas.append(aresta)
                                     
+def adicionaCor(vertice):
+    possiveis = list(cores) 
+    for v in vertice.adj:
+        if v.dia != 0:
+            possiveis.remove(v.dia)
+    possiveis.sort()
+    vertice.dia = possiveis[0]
             
 def weshPowell(vList):
-    g = sorted(vList)
+    g = sorted(vList,key = lambda vertice: len(vertice.adj),reverse=True )
+    for v in g:
+        adicionaCor(v)
     
-        
+    fileRes = open('cronograma.txt','w')
+    
+    
+    i = 0
+    lines = ''
+    while i < len(cores):
+        diaVazio = True        
+        lines = lines + f'Bandas dias {i}:\n'
+        diaAtual = ''
+        for v in g:
+            if v.dia == i:
+                diaAtual = diaAtual + f'   {v.id}\n'
+                diaVazio = False
+        if not diaVazio:
+            lines = lines + diaAtual
+        i+=1
+    fileRes.write(lines)
+    fileRes.close     
 
 weshPowell(vertices)
             
